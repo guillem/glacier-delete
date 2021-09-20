794a510753dc48e28743b35400f77210332b889c
Marco <meskatech@gmail.com>
2021-09-20 14:10:15 +0200

    Update 2_get_archive_ids.sh
    Fix for jobs ids starting with a dash

--- 2_get_archive_ids.sh
+++ 2_get_archive_ids.sh
@@ -4,5 +4,5 @@
 MY_INPUT=job_status.json
 
 for X in `jq -c '[.JobList[].VaultARN,.JobList[].JobId]' $MY_INPUT | tr -d '[]"' | cut -d/ -f2`; do IFS=','; set -- $X;
-	aws glacier get-job-output --account-id $ACCOUNT_ID --vault-name $1 --job-id $2 $1.ids
+	aws glacier get-job-output --account-id $ACCOUNT_ID --vault-name $1 --job-id="$2" $1.ids
 done
